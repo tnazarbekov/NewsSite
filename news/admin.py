b17e3188e14f939aa2d9a617b4261b0079cbb024
tnazarbekov <tnazarbekov@bk.ru>
2022-04-15 13:54:34 +0600

    admin panel customization

---
+++ news/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django import forms
+from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+"""FORM for ckeeditor(news add functionality)"""
+
+
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
+"""Customization for our admin panel"""
+
+
+class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
+
+
+"""Customization for category panel"""
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
